eb6619f6f68b908095cd8fcb44b6d79e0dad4feb
karim33elabasy <karim33elabasy@gmail.com>
2024-07-05 12:20:04 +0300

    added features folders

---
+++ lib/features/splash_onboarding/presentation/views/onboarding_03_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Onboarding03 extends StatelessWidget {
+  const Onboarding03({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
