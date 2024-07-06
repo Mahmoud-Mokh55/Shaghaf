efef04e41d5ccf1d500729bdaef008ed94272492
OmarEldmohi <omareldmohi@gmail.com>
2024-07-06 18:05:32 +0300

    Finished Styles.dart FInished AppRouter.dart

--- lib/core/utils/Styles.dart
+++ lib/core/utils/Styles.dart
@@ -2,82 +2,88 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-
-
-
-TextStyle SharedStyle = const TextStyle(
-  fontFamily: 'Comfortaa',
-    letterSpacing: -0.3
-);
-
-
-
+TextStyle SharedStyle =
+    const TextStyle(fontFamily: 'Comfortaa', letterSpacing: -0.3);
 
 //------------------------\\
 //Splash FONT STYLES
 //------------------------\\
 
-TextStyle SPLASH_T_title = SharedStyle.copyWith(
-    fontSize: 20,
-    color: Colors.white,
-    fontWeight: FontWeight.w600,
+TextStyle s20_white_bold = SharedStyle.copyWith(
+  fontSize: 20,
+  color: Colors.white,
+  fontWeight: FontWeight.w600,
 );
 
-TextStyle SPLASH_T_paragraph = SharedStyle.copyWith(
-    fontSize: 16,
-    color: Color(0xFFFFC100),
-    fontWeight: FontWeight.w400,
+TextStyle s16_yellow = SharedStyle.copyWith(
+  fontSize: 16,
+  color: Color(0xFFFFC100),
+  fontWeight: FontWeight.w400,
 );
 
-TextStyle SPLASH_T_skip = SharedStyle.copyWith(
-    fontSize: 16,
-    color: Colors.white,
-    fontWeight: FontWeight.w400,
+TextStyle s16_white = SharedStyle.copyWith(
+  fontSize: 16,
+  color: Colors.white,
+  fontWeight: FontWeight.w400,
 );
-
-
-
 
 //------------------------\\
 // Auth FONT STYLES
 //------------------------\\
+TextStyle s10_lightGrey = SharedStyle.copyWith(
+  fontSize: 10,
+  color: Color(0xFF969696),
+  fontWeight: FontWeight.w400,
+);
 
-TextStyle AUTH_T_buttonLabel = SharedStyle.copyWith(
+TextStyle s12_white = SharedStyle.copyWith(
+  fontSize: 12,
+  color: Colors.white,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle s12_lightGrey = SharedStyle.copyWith(
     fontSize: 12,
+    color: Color(0xFF787878),
+    fontWeight: FontWeight.w400,
+);
+
+TextStyle s12_red = SharedStyle.copyWith(
+  fontSize: 12,
+  color: Color(0xFFF04C29),
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle s12_black = SharedStyle.copyWith(
+    fontSize: 12,
+    color: Color(0xFF101623),
+    fontWeight: FontWeight.w400,
+);
+
+TextStyle s14_darkGrey = SharedStyle.copyWith(
+  fontSize: 14,
+  color: Color(0xFF2F2F2F),
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle s16_lightGrey = SharedStyle.copyWith(
+    fontSize: 16,
+    color: Color(0xFFA1A8B0),
+    fontWeight: FontWeight.w400,
+);
+
+
+TextStyle s17_white = SharedStyle.copyWith(
+    fontSize: 17,
     color: Colors.white,
     fontWeight: FontWeight.w500,
 );
 
-TextStyle AUTH_T_textInputLabel = SharedStyle.copyWith(
-    fontSize: 14,
-    color: Color(0xFF2F2F2F),
-    fontWeight: FontWeight.w400,
+TextStyle s20_black_bold = SharedStyle.copyWith(
+    fontSize: 20,
+    color: Color(0xFF101623),
+    fontWeight: FontWeight.w700,
 );
-
-TextStyle AUTH_T_textInputHint = SharedStyle.copyWith(
-    fontSize: 10,
-    color: Color(0xFF969696),
-    fontWeight: FontWeight.w400,
-);
-//.
-
-TextStyle AUTH_T_red = SharedStyle.copyWith(
-    fontSize: 12,
-    color: Color(0xFFF04C29),
-    fontWeight: FontWeight.w500,
-);
-
-TextStyle AUTH_T_black = SharedStyle.copyWith(
-  color: Color(0xFF101623),
-  fontWeight: FontWeight.w600,
-);
-
-TextStyle AUTH_T_greySubTitle = SharedStyle.copyWith(
-  color: Color(0xFF787878),
-  fontWeight: FontWeight.w400,
-);
-
-
 
 
 
@@ -85,10 +91,152 @@ TextStyle AUTH_T_greySubTitle = SharedStyle.copyWith(
 // Home FONT STYLES
 //------------------------\\
 
-ButtonStyle Home_redButton = ButtonStyle(
-
+TextStyle s20_grey = SharedStyle.copyWith(
+    fontSize: 20,
+    color: Color(0xFF383838),
+    fontWeight: FontWeight.w400,
 );
 
-TextStyle HOME_T_buttonLabel = SharedStyle.copyWith(
-  color: Colors.white
+TextStyle s10_olive = SharedStyle.copyWith(
+    fontSize: 20,
+    color: Color(0xFF5E5F60),
+    fontWeight: FontWeight.w400,
+);
+
+TextStyle s16_black = SharedStyle.copyWith(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+);
+
+TextStyle s18_black = SharedStyle.copyWith(
+    fontSize: 18,
+    color: Color(0xFF1B1B1B),
+    fontWeight: FontWeight.w500,
+);
+
+TextStyle s20_black = SharedStyle.copyWith(
+    fontSize: 20,
+    color: Color(0xFF111111),
+    fontWeight: FontWeight.w500,
+);
+
+
+
+TextStyle s14_red = SharedStyle.copyWith(
+    fontSize: 14,
+    color: Color(0xFFF04C29),
+    fontWeight: FontWeight.w400,
+);
+
+TextStyle s16_red_bold = SharedStyle.copyWith(
+    fontSize: 16,
+    color: Color(0xFFF04C29),
+    fontWeight: FontWeight.w600,
+);
+
+TextStyle s18_red_bold = SharedStyle.copyWith(
+    fontSize: 18,
+    color: Color(0xFFF04C29),
+    fontWeight: FontWeight.w600,
+);
+
+TextStyle s14_grey = SharedStyle.copyWith(
+    fontSize: 14,
+    color: Color(0xFF515151),
+    fontWeight: FontWeight.w400,
+);
+
+
+TextStyle s12_grey = SharedStyle.copyWith(
+    fontSize: 12,
+    color: Color(0xFF515151),
+    fontWeight: FontWeight.w400,
+);
+
+
+//------------------------\\
+// Booking FONT STYLES
+//------------------------\\
+
+TextStyle s14_black = SharedStyle.copyWith(
+    fontSize: 15,
+    color: Color(0xFF000000),
+    fontWeight: FontWeight.w500,
+);
+
+
+TextStyle s15_black = SharedStyle.copyWith(
+    fontSize: 15,
+    color: Color(0xFF000000),
+    fontWeight: FontWeight.w500,
+);
+
+TextStyle s16_lightGreen = SharedStyle.copyWith(
+    fontSize: 16,
+    color: Color(0xFF455A64),
+    fontWeight: FontWeight.w500,
+);
+
+
+//------------------------\\
+// Offers and Events FONT STYLES
+//------------------------\\
+
+TextStyle s11_darkGrey = SharedStyle.copyWith(
+    fontSize: 11,
+    color: Color(0xFF252525),
+    fontWeight: FontWeight.w500,
+);
+
+TextStyle s14_green = SharedStyle.copyWith(
+    fontSize: 14,
+    color: Color(0xFF20473E),
+    fontWeight: FontWeight.w500,
+);
+
+TextStyle s13_green = SharedStyle.copyWith(
+    fontSize: 13,
+    color: Color(0xFF20473E),
+    fontWeight: FontWeight.w400,
+);
+
+
+//------------------------\\
+// Membership FONT STYLES
+//------------------------\\
+
+TextStyle s12_black_bold = SharedStyle.copyWith(
+    fontSize: 12,
+    color: Color(0xFF101623),
+    fontWeight: FontWeight.w700,
+);
+
+TextStyle s16_white_bold = SharedStyle.copyWith(
+    fontSize: 16,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+);
+
+TextStyle s10_grey = SharedStyle.copyWith(
+    fontSize: 10,
+    color: Color(0xFF464646),
+    fontWeight: FontWeight.w400,
+);
+
+
+//------------------------\\
+// Settings FONT STYLES
+//------------------------\\
+
+TextStyle s16_black_bold = SharedStyle.copyWith(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+);
+
+TextStyle s20_softerBlack = SharedStyle.copyWith(
+    fontSize: 20,
+    color: Color(0xFF191919),
+    fontWeight: FontWeight.w500,
 );
