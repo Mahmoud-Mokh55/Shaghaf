9797fd956ddeb0876d1280e9af96d8862fd8d712
karim33elabasy <karim33elabasy@gmail.com>
2024-07-10 14:58:23 +0300

    made splash screen completely

--- lib/features/splash_onboarding/presentation/views/onboarding_01_screen.dart
+++ lib/features/splash_onboarding/presentation/views/onboarding_01_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shaghaf/core/utils/app_colors.dart';
+import 'package:shaghaf/core/utils/styles.dart';
 
 class Onboarding01 extends StatelessWidget {
   const Onboarding01({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: AppColors.kBackColor,
+      body: SafeArea(
+        child: OnboardingScreenBody()
+      ),
+    );
   }
 }
+class OnboardingScreenBody extends StatelessWidget {
+  const OnboardingScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: (){},
+            child: Text("Skip",style: Styles.s17_white,),
+          ),
+        ),
+
+      ],
+    );
+  }
+}
+
