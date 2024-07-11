94bae16c1e4292572417cfc92748567681f3a715
karim33elabasy <karim33elabasy@gmail.com>
2024-07-11 13:18:07 +0300

    building authentication login_screen_body.dart

--- lib/core/widgets/my_tff.dart
+++ lib/core/widgets/my_tff.dart
@@ -5,26 +5,32 @@ import 'package:shaghaf/core/utils/assets.dart';
 import 'package:shaghaf/core/utils/styles.dart';
 
 class MyTff extends StatelessWidget {
+  final String title;
   final bool obscureText;
   final Widget prefixIcon;
   final String hintText;
-  const MyTff({super.key, required this.obscureText, required this.prefixIcon, required this.hintText});
+  const MyTff({super.key, required this.title, required this.obscureText, required this.prefixIcon, required this.hintText});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          gapPadding: 10
+    return Column(
+      children: [
+        Text(title),
+        TextFormField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              gapPadding: 10
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            prefixIcon: prefixIcon,
+            hintText: hintText,
+            hintStyle: Styles.s10_lightGrey
+          ),
         ),
-        filled: true,
-        fillColor: Colors.white,
-        prefixIcon: prefixIcon,
-        hintText: hintText,
-        hintStyle: Styles.s10_lightGrey
-      ),
+      ],
     );
   }
 }
--- lib/features/authentication/presentation/views/widgets/login_screen_body.dart
+++ lib/features/authentication/presentation/views/widgets/login_screen_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shaghaf/core/utils/app_colors.dart';
 import 'package:shaghaf/core/utils/assets.dart';
 import 'package:shaghaf/core/widgets/my_tff.dart';
 import 'package:shaghaf/features/authentication/presentation/views/widgets/yellow_container.dart';
@@ -16,7 +17,8 @@ class LoginScreenBody extends StatelessWidget {
         YellowContainer(
           child: Column(
             children: [
-              MyTff(obscureText: false, prefixIcon: Image.asset(Assets.phoneIcon,scale: 3.5,color: AppColors.kfadeGrey,), hintText: "Enter your phone number")
+              MyTff(obscureText: false, prefixIcon: Image.asset(Assets.phoneIcon,scale: 3.5,color: AppColors.kfadeGrey,), hintText: "Enter your phone number"),
+              MyTff(obscureText: true, prefixIcon: Image.asset(Assets.passIcon,scale: 3.5,color: AppColors.kfadeGrey,), hintText: "Enter your password"),
             ],
           )
         )
